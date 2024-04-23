fad41226b2a4368c4bd1afc04c8dae52e9c0445b
91990 <sreenevas545@gmail.com>
2024-04-23 13:46:38 +0530

    improve1

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Dande # no space between =
+Person2=Raj
+
+echo "Dande:Hello Raj how are you?"
+echo "Raj:I am doing good, thanks for asking. How are you?"
+echo "Dande:I am fine, I am learning devops How is your job?"
+echo "Raj: My job is good but not much hike this time"
