a51cbb3b52e3c6d9061915989405930aaaa9d64f
91990 <sreenevas545@gmail.com>
2024-04-23 14:20:25 +0530

    cred

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username"
+
+read USERNAME # username is variable
+
+echo "Please enter password"
+
+read PASSWORD # PASSWORD is variable
+
+echo "username is: $USERNAME, password is:$PASSWORD"
