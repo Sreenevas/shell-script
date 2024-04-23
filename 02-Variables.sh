a50bea8d548ee61c0b55da3cabfe7ad8200872b0
91990 <sreenevas545@gmail.com>
2024-04-23 13:40:44 +0530

    minor

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Dande::Hello Raj how are you?"
-echo "Raj::I am doing good, thanks for asking. How are you?"
-echo "Dande::I am fine, I am learning devops How is your job?"
-echo "Raj:: My job is good but not much hike this time"
+echo "Dande:Hello Raj how are you?"
+echo "Raj:I am doing good, thanks for asking. How are you?"
+echo "Dande:I am fine, I am learning devops How is your job?"
+echo "Raj: My job is good but not much hike this time"
