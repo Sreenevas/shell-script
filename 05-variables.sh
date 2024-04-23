f5d4fff21dcb3c30704a68f98ad9fbf9c7656dbc
91990 <sreenevas545@gmail.com>
2024-04-23 14:22:21 +0530

    hidecred

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter username"
 
-read USERNAME # username is variable
+read -s USERNAME # username is variable
 
 echo "Please enter password"
 
-read PASSWORD # PASSWORD is variable
+read -s PASSWORD # PASSWORD is variable
 
 echo "username is: $USERNAME, password is:$PASSWORD"
