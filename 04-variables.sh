569641e3a49e9b01d10fb9e234366c78c6858fd4
91990 <sreenevas545@gmail.com>
2024-04-23 14:05:18 +0530

    added file

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Declaring of variables
+Person1=$1
+Person2=$2
+
+#referring  variables
+echo "$Person1:: Hi $Person2, Welcome to devops class. How are you?"
+echo "$Person2:: I am good $Person1, Thanks for your wishes. H r u?"
+echo "$Person1:: I am doing good, Hope $Person2 you will get job"
