c10313c58e8cdd83474097504a71d4f3bff13992
91990 <sreenevas545@gmail.com>
2024-04-23 13:52:43 +0530

    changes names

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-Person1=Pavan # no space between =
-Person2=Raj
+Person1=Arvind # no space between =
+Person2=Umesh
 
-echo "$Person1:Hello Raj how are you?"
-echo "$Person2:I am doing good, thanks for asking. How are you?"
-echo "$Person1:I am fine, I am learning devops How is your job?"
-echo "$Person2: My job is good but not much hike this time"
+echo "$Person1 :: Hello Raj how are you?"
+echo "$Person2 :: I am doing good, thanks for asking. How are you?"
+echo "$Person1 :: I am fine, I am learning devops How is your job?"
+echo "$Person2 :: My job is good but not much hike this time"
